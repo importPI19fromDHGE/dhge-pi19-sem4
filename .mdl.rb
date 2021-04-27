226814e5f90cd62c1db2c313f0b9658ee2fd1437
Yannis Becker <yannisbecker.dev@gmail.com>
2021-04-27 15:03:24 +0200

    Update .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -1,14 +1,2 @@
-# Enable all rules by default
-all
-
-# Extend line length, since each sentence should be on a separate line.
-rule 'MD013', :line_length => 99999
-
-# Allow in-line HTML
-exclude_rule 'MD033'
-
 # Allow any Heading styles
 exclude_rule 'MD003' 
-
-# Nested lists should be indented with four spaces.
-rule 'MD007', :indent => 4
