d4acbc4bf08ac56b5111effcabc60264816e42f0
Yannis Becker <yannisbecker.dev@gmail.com>
2021-04-27 15:18:55 +0200

    Update .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -7,5 +7,8 @@ exclude_rule 'MD003'
 # First line of the file is not required to be the top-level heading
 exclude_rule 'MD041'
 
+# Allow the use of emphasis for small-headers
+exclude_rule 'MD036'
+
 # Extend line length, since each sentence should be on a separate line.
 rule 'MD013', :line_length => 120
